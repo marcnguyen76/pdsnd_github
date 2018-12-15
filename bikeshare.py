ad47b22ac1c3b80933f40b153f7506b659d6b14e
Nguyen <J37451@NGC.AUTHORITY>
2018-12-14 16:12:59 -0800

    Add info about project on header of bikeshare.py

--- bikeshare.py
+++ bikeshare.py
@@ -1,5 +1,7 @@
-# Marc Nguyen
-
+""" In this project, you will use data provided by Motivate, a bike share system provider for many major cities 
+in the United States, to uncover bike share usage patterns. You will compare the system usage between three large cities: 
+Chicago, New York City, and Washington, DC.
+"""
 import time
 import pandas as pd
 import numpy as np
